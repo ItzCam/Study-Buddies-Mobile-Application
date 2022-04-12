c99a6c19e85ab5ef7212b8e87d1d81978310f1b5
zero7670 <94492858+zero7670@users.noreply.github.com>
2022-04-11 23:53:34 -0700

    Added Calculator and To do List

--- FirebaseAuth/MyApp/App.js
+++ FirebaseAuth/MyApp/App.js
@@ -1,68 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+//@refresh state
 import React from 'react';
-import {
-  Link,
-  Text,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  VStack,
-  Code,
-} from 'native-base';
-import NativeBaseIcon from './src/components/NativeBaseIcon';
+import { StatusBar } from 'expo-status-bar';
+import {ImageBackground, StyleSheet, Text, View, Platform, LogBox} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import CalculatorScreen from './screens/CalculatorScreen';
+import TitleScreen from './components/TitleComponents/TitleScreen';
+import LoginScreen from "./components/TitleComponents/LoginScreen";
+import SignUpScreen from "./components/TitleComponents/SignUpScreen";
+import CreateProfileScreen from "./components/TitleComponents/CreateProfileScreen";
+import ForgotPasswordScreen from './components/TitleComponents/ForgotPasswordScreen'
+import ScheduleScreen from "./components/ScheduleScreen";
+import VolunteersScreen from "./components/SMDashboardComponenents/VolunteersScreen";
+import CreateMeetingScreen from "./components/SMDashboardComponenents/CreateMeetingScreen";
+import ProfileScreen from "./components/SMDashboardComponenents/ProfileScreen";
+import EditProfileScreen from './components/SMDashboardComponenents/EditProfileScreen';
 
-// Color Switch Component
-function ToggleDarkMode() {
-  const {colorMode, toggleColorMode} = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === 'light' ? true : false}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === 'light' ? 'switch to dark mode' : 'switch to light mode'
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function SMDashboardTabs() {
+    return (
+        <Tab.Navigator screenOptions={{tabBarActiveTintColor: "#302f90", tabBarLabelStyle: {fontSize: 11, fontWeight: "bold"}}}>
+            <Tab.Screen name="Schedule" component={ScheduleScreen} options={{ tabBarIcon: () => (
+                <FontAwesome name="calendar" color="#333333" size={25}/>),}}/>
+            <Tab.Screen name="Create Meeting" component={CreateMeetingScreen} options={{ tabBarIcon: () => (
+                    <FontAwesome name="pencil" color="#333333" size={25}/>),}}/>
+            <Tab.Screen name="Buddy List" component={VolunteersScreen} options={{ tabBarIcon: () => (
+                <FontAwesome name="users" color="#333333" size={25}/>),}}/>
+            <Tab.Screen name="Profile" component={ProfileStack} options={{headerShown: false, tabBarIcon: () => (
+                <FontAwesome name="user" color="#333333" size={25}/>),}}/>
+        </Tab.Navigator>
+    );
 }
-const App = () => {
-  return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{bg: 'blueGray.900'}}
-        _light={{bg: 'blueGray.50'}}
-        px={4}
-        flex={1}>
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Code>App.js</Code>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={'xl'}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
-    </NativeBaseProvider>
-  );
-};
+
+function VolunteerTabs() {
+    return (
+        <Tab.Navigator tabBarOptions={{activeTintColor: '#302f90', labelStyle: {fontSize: 11, fontWeight: 'bold'}}}>
+            <Tab.Screen name="Schedule" component={ScheduleScreen} options={{ tabBarIcon: () => (
+                <FontAwesome name="calendar" color="#333333" size={25}/>),}}/>
+            <Tab.Screen name="Volunteers" component={VolunteersScreen} options={{ tabBarIcon: () => (
+                <FontAwesome name="users" color="#333333" size={25}/>),}}/>
+            <Tab.Screen name="Profile" component={ProfileStack} options={{headerShown: false, tabBarIcon: () => (
+                <FontAwesome name="user" color="#333333" size={25}/>),}}/>
+        </Tab.Navigator>
+    );
+}
+
+function ProfileStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name ="Profile Screen" component={ProfileScreen}/>
+            <Stack.Screen name ="Edit Profile" component={EditProfileScreen}/>
+            <Stack.Screen name = "Calculator" component={CalculatorScreen}/>
+        </Stack.Navigator>
+    );
+}
+
+
+function App() {
+    LogBox.ignoreAllLogs();
+    return (
+        <NavigationContainer style={styles.container}>
+            <Stack.Navigator initialRouteName={'Title'}>
+                <Stack.Screen name="Title" component={TitleScreen} options={{headerShown: false}}/>
+                <Stack.Screen name="Sign Up" component={SignUpScreen}/>
+                <Stack.Screen name="Create Profile" component={CreateProfileScreen} options={{headerShown: false}}/>
+                <Stack.Screen name="Login" component={LoginScreen}/>
+                <Stack.Screen name="Forgot Password" component={ForgotPasswordScreen}/>
+                <Stack.Screen name="SM Dashboard" component={SMDashboardTabs} options={{headerShown: false}}/>
+                <Stack.Screen name="Volunteer Dashboard" component={VolunteerTabs} options={{headerShown: false}}/>
+            </Stack.Navigator>
+
+            {/*<StatusBar style="auto"/>*/}
+        </NavigationContainer>
+
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 export default App;
